470cbf90d4aa69777429ae3d31f1a712b24f9247
ShreyaShendkar <shreyashendkar8@gmail.com>
2025-12-22 20:57:06 +0530

    Add Transaction component with dynamic amount display

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { GlobalContext } from '../context/GlobalState';
+import { Transaction } from './Transaction';
 
 export const TransactionList = () => {
   const { transactions } = useContext(GlobalContext);
@@ -10,11 +11,7 @@ export const TransactionList = () => {
    <>
      <h3>History</h3>
      <ul className="list">
-      {transactions.map(transaction => (<li className="minus"> 
-        {transaction.text}
-        Cash <span>-₹400</span><button className="delete-btn">x</button>
-        </li>
-      ))}
+      {transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction} />))}
      
      </ul>
    </>
