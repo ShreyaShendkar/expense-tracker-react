86b7ab47b2eb4e410ac081b4914a7b7584efb2b8
ShreyaShendkar <shreyashendkar8@gmail.com>
2025-12-22 20:10:23 +0530

    Create expense tracker components structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Header } from './components/Header.js';
+import { Balance } from './components/Balance.js';
+import { IncomeExpenses } from './components/IncomeExpenses.js';
+import { TransactionList } from './components/TransactionList.js';
 import './App.css';
+import { AddTransaction } from './components/AddTransaction.js';
 
 function App() {
   return (
-    <>
-    Hello world
-    </>
+    <div>
+     <Header />
+     <div className='container'>
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
+    </div>
   );
 }
 
